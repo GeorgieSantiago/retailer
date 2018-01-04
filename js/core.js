e7bd05a0a725476ca0e7db7dc639d76db15e7c3a
GeorgieSantiago <milestoneartisan@gmail.com>
2018-01-03 22:11:13 -0500

    Create dataobject on init and get data from the pipeline api

--- js/core.js
+++ js/core.js
@@ -4,7 +4,6 @@ var myApp = new Framework7({
     //uniqueHistory: true
 });
 
-
 // If we need to use custom DOM library, let's save it to $$ variable:
 var $$ = Dom7;
 
@@ -15,7 +14,7 @@ var mainView = myApp.addView('.view-main', {
 });
 
 // Handle Cordova Device Ready Event
-$$(document).on('deviceready', function() {
+$$(document).on('deviceready', function () {
     console.log("Device is ready!");
 });
 
@@ -27,14 +26,70 @@ myApp.onPageInit('about', function (page) {
     console.log(page);
 });
 
-window.addEventListener('error', function(e) {
-    alert(e);
+window.addEventListener('error', function (e) {
     console.log(e);
 }, true);
 
-Router = {
-    RouteFactory: function(page) {
+var Router = {
+    RouteFactory: function (page) {
         myApp.initPage(page);
         console.log(page);
     }
+};
+
+var API = {
+    base_url: "https://pipeline-api.000webhostapp.com/www/pipeline/index.php",
+    get: function (url , body) {
+        //do get request
+        $$.get(url, body, function (data) {
+            console.log('Request Sent: ');
+            console.log(body);
+            API.dataFactory(data);
+        });
+    },
+
+    post: function (url , body) {
+        //do post request
+        $$.post(url, body, function (data) {
+            console.log('Request Sent: ');
+            console.log(body);
+            API.dataFactory(data);
+        });
+    },
+
+    request: function()
+    {
+        //get JSON request
+        $$.getJSON(url, function (json) {
+            console.log(json);
+        });
+    },
+
+    dataFactory: function(data)
+    {
+        console.log(data);
+        USER.push(data);
+    },
+    /*TODO Special Controller request type User*/
+    /*TODO pass all user credentials and create login/registration system :D maybe even
+    * sync some Apps: Facebook, Google, ya know the works!*/
+    init: function()
+    {
+        this.post(API.base_url , {
+            'auth' : '12345',
+            'request_type' : 'User',
+            'table' : 'test'
+        })
+    }
+
 }
+
+
+
+//Post AppInit Calls
+
+// Data Constant for moving app data internally
+const USER = [];
+
+// Initialize the data call
+API.init();
